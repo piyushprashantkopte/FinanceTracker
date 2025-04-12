597b1bbf65aeb629761edaebe7f216eb202c6d56
piyushprashantkopte <piyushkopte23@gmail.com>
2025-04-12 13:56:39 +0530

    change database from mongo to sqlite3

--- financetracker/settings.py
+++ financetracker/settings.py
@@ -75,25 +75,25 @@ WSGI_APPLICATION = 'financetracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': os.environ['DB_NAME'],  # Enforce required vars,
-        'CLIENT': {
-            'host': os.getenv('DATABASE_URL'),
-            'username': 'piyushkopte',
-            'password': '19782003',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'djongo',
+#        'NAME': os.environ['DB_NAME'],  # Enforce required vars,
+#        'CLIENT': {
+#            'host': os.getenv('DATABASE_URL'),
+#            'username': 'piyushkopte',
+#            'password': '19782003',
+#        }
+#    }
+#}
 
 
 # Password validation
